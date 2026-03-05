275da6a14ac73db25e338327c848e5e2bb2b8ea0
Adam Burdette <alburdet619@gmail.com>
2026-03-05 11:30:48 -0500

    Styling Game History screen
    Added styles to the new Game history screen and added a back button.

--- src/screens/GameHistory/index.tsx
+++ src/screens/GameHistory/index.tsx
@@ -1,8 +1,11 @@
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { useNavigation } from "@react-navigation/native";
 import { format } from "date-fns";
 import { useCallback } from "react";
 import {
   FlatList,
   ListRenderItemInfo,
+  Pressable,
   StyleSheet,
   Text,
   View,
@@ -14,6 +17,8 @@ import { Colors } from "../../styles/colors";
 import { HistoryType } from "../../types/ticTacToeTypes";
 
 export const GameHistory = () => {
+  const { goBack } = useNavigation();
+
   const { history } = useHistoryStore();
 
   const renderHistoryItem = useCallback(
@@ -25,29 +30,46 @@ export const GameHistory = () => {
 
       return (
         <View style={styles.historyItemContainer}>
-          <Text>{item.result}</Text>
-          <Text>{formattedTimestamp}</Text>
+          <Text style={styles.historyItemText}>{item.result}</Text>
+          <Text style={styles.historyItemText}>{formattedTimestamp}</Text>
         </View>
       );
     },
     [],
   );
 
+  const renderItemSeparator = useCallback(
+    () => <View style={styles.separator} />,
+    [],
+  );
+
   return (
     <SafeAreaView style={styles.container}>
+      <Pressable onPress={goBack} style={styles.backButton}>
+        <MaterialIcons color={Colors.sky} name="arrow-back" size={24} />
+      </Pressable>
       <FlatList
         alwaysBounceVertical={false}
+        contentContainerStyle={styles.historyListContainer}
         data={history}
+        ItemSeparatorComponent={renderItemSeparator}
         keyExtractor={(item) =>
           `${item.result}_${new Date(item.timestamp).toISOString()}`
         }
         renderItem={renderHistoryItem}
+        style={styles.historyList}
       />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  backButton: {
+    alignSelf: "flex-start",
+    backgroundColor: Colors.coral,
+    borderRadius: 24,
+    padding: 8,
+  },
   container: {
     backgroundColor: Colors.primary,
     flex: 1,
@@ -56,5 +78,22 @@ const styles = StyleSheet.create({
   historyItemContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
+    paddingVertical: 12,
+  },
+  historyItemText: {
+    color: Colors.sky,
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  historyList: {
+    marginTop: 24,
+  },
+  historyListContainer: {
+    flexGrow: 1,
+  },
+  separator: {
+    backgroundColor: Colors.gold,
+    height: 1,
+    width: "100%",
   },
 });
